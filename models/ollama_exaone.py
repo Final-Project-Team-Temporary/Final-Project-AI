df5dc487605841bbc4c16805bff0f7934dd99767
mirupio <162397816+mirupio@users.noreply.github.com>
2025-05-27 02:14:04 +0900

    [FEATURE] LangServe 기반 summarizer_chain API 구현 및 등록

---
+++ models/ollama_exaone.py
@@ -0,0 +1,3 @@
+from langchain_community.llms import Ollama
+
+exaone_llm = Ollama(model="exaone3.5:7.8b")
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from chains.summarizer import summarizer_chain
 
 app = FastAPI()
 
@@ -10,8 +11,7 @@ async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
 
-# Edit this to add the chain you want to add
-#add_routes(app, NotImplemented)
+add_routes(app, summarizer_chain, path="/summarize")
 
 if __name__ == "__main__":
     import uvicorn
